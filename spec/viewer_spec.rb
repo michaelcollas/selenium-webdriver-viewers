b60d8ff98692bb229c1bf0737b2fb2f012973b4d
Michael Collas <mcollas@yahoo.com>
2010-12-20 15:04:08 +1100

    patched broken spec

--- spec/viewer_spec.rb
+++ spec/viewer_spec.rb
@@ -6,7 +6,18 @@ describe WebViewer, 'included in a class' do
     @viewer_class = Class.new(Object).extend(WebViewer)
     @viewer = @viewer_class.new
     @fake_browser = stub
-    @viewer.stub(browser).and_return(@fake_browser)
+    @viewer.stub(:browser).and_return(@fake_browser)
+  end
+
+  describe '#base_element' do
+
+    it 'should return the base element if one has been assigned' do
+      pending
+      fake_element = stub('fake element')
+      @viewer.base_element = fake_element
+      @viewer.base_element.should == fake_element
+    end
+
   end
 
 end
